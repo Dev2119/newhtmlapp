41076e416519e89726fcff6377e8412415dc34d9
theAD-dev <dev@thead.com.au>
2026-02-09 14:23:16 +0530

    smartsupp1

--- public/smartsupp.js
+++ public/smartsupp.js
@@ -1,19 +1,19 @@
-// var _smartsupp = _smartsupp || {};
-// _smartsupp.key = '2996e5ea3ca8fbed95d0eb2aa8ef3007521fcb1e';
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '2996e5ea3ca8fbed95d0eb2aa8ef3007521fcb1e';
 
-// window.smartsupp || (function (d) {
-//   var s, c, o = smartsupp = function () {
-//     o._.push(arguments);
-//   };
-//   o._ = [];
-//   s = d.getElementsByTagName('script')[0];
-//   c = d.createElement('script');
-//   c.type = 'text/javascript';
-//   c.charset = 'utf-8';
-//   c.async = true;
-//   c.src = 'https://www.smartsuppchat.com/loader.js?';
-//   s.parentNode.insertBefore(c, s);
-// })(document);
+window.smartsupp || (function (d) {
+  var s, c, o = smartsupp = function () {
+    o._.push(arguments);
+  };
+  o._ = [];
+  s = d.getElementsByTagName('script')[0];
+  c = d.createElement('script');
+  c.type = 'text/javascript';
+  c.charset = 'utf-8';
+  c.async = true;
+  c.src = 'https://www.smartsuppchat.com/loader.js?';
+  s.parentNode.insertBefore(c, s);
+})(document);
 
 
 
